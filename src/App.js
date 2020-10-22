ffb4dc153c0f5b43f354dc7b398ec656876d15b1
Ken Charette <charettekenneth@gmail.com>
2020-10-21 20:58:01 -0400

    Initial setup with map displaying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+/* Imports */
+import React, { useLayoutEffect } from 'react';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4maps from "@amcharts/amcharts4/maps";
+import am4geodata_usaLow from "@amcharts/amcharts4-geodata/usaLow";
+import am4themes_spiritedaway from "@amcharts/amcharts4/themes/spiritedaway";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import './App.css';
 
-function App() {
+const App = (props) => {
+
+  useLayoutEffect(() => {
+    
+    /* Chart code */
+    // Themes begin
+    am4core.useTheme(am4themes_spiritedaway);
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+
+    // Create map instance
+    let chart = am4core.create("chartdiv", am4maps.MapChart);
+    chart.maxZoomLevel = 64;
+
+    // Set map definition
+    chart.geodata = am4geodata_usaLow;
+
+    // Set projection
+    chart.projection = new am4maps.projections.AlbersUsa();
+
+    // Create map polygon series
+    let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    polygonSeries.useGeodata = true;
+    polygonSeries.calculateVisualCenter = true;
+
+    // Configure series
+    let polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name}";
+    polygonTemplate.fill = chart.colors.getIndex(0);
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
   );
 }
 
 export default App;
+
+
+
+
